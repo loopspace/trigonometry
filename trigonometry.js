f168eee511745ee2df5ac3b7f99dfd8f484491bf
Andrew Stacey <stacey@math.ntnu.no>
2015-02-03 21:22:31 +0000

    Added "wrap" for when the angle goes off the graph, added option to display or not display sine and cosine, added angle display.

--- trigonometry.js
+++ trigonometry.js
@@ -1,8 +1,16 @@
 var ctx;
 var ang = Math.PI/4;
 var clicked;
+var angdisplay;
+var sbox;
+var cbox;
 
 function init() {
+    angdisplay = document.querySelector('#ang');
+    sbox = document.querySelector('#sine');
+    cbox = document.querySelector('#cosine');
+    sbox.onchange = draw;
+    cbox.onchange = draw;
     var g = document.querySelector("#trig");
     g.addEventListener("mousedown",doMouseDown,false);
     g.addEventListener("mouseup",doMouseUp,false);
@@ -39,25 +47,31 @@ function draw() {
     ctx.lineTo(width/2,0);
     ctx.stroke();
 
-    ctx.strokeStyle = "red";
-    ctx.beginPath();
-    n = 1000;
-    dx = width/(50*n);
-    ctx.moveTo(-width/2,radius*Math.sin(-n*dx));
-    for (i=-n+1; i< n; i++) {
-	ctx.lineTo(freq*i*dx,radius*Math.sin(i*dx));
-    }
-    ctx.stroke();
+    if (sbox.checked) {
 
-    ctx.strokeStyle = "blue";
-    ctx.beginPath();
-    n = 1000;
-    dx = height/(50*n);
-    ctx.moveTo(radius*Math.cos(-n*dx),-height/2);
-    for (i=-n+1; i< n; i++) {
-	ctx.lineTo(radius*Math.cos(i*dx),freq*i*dx);
+	ctx.strokeStyle = "red";
+	ctx.beginPath();
+	n = 1000;
+	dx = width/(50*n);
+	ctx.moveTo(-width/2,radius*Math.sin(-n*dx));
+	for (i=-n+1; i< n; i++) {
+	    ctx.lineTo(freq*i*dx,radius*Math.sin(i*dx));
+	}
+	ctx.stroke();
     }
-    ctx.stroke();
+
+    if (cbox.checked) {
+    
+	ctx.strokeStyle = "blue";
+	ctx.beginPath();
+	n = 1000;
+	dx = height/(50*n);
+	ctx.moveTo(radius*Math.cos(-n*dx),-height/2);
+	for (i=-n+1; i< n; i++) {
+	    ctx.lineTo(radius*Math.cos(i*dx),freq*i*dx);
+	}
+	ctx.stroke();
+    }
 
     ctx.strokeStyle = "black";
     ctx.beginPath();
@@ -67,11 +81,17 @@ function draw() {
 
     ctx.setLineDash([2,2]);
     ctx.beginPath();
-    ctx.moveTo(freq*ang,0);
-    ctx.lineTo(freq*ang,radius*Math.sin(ang));
-    ctx.lineTo(radius*Math.cos(ang),radius*Math.sin(ang));
-    ctx.lineTo(radius*Math.cos(ang),freq*ang);
-    ctx.lineTo(0,freq*ang);
+    if (sbox.checked) {
+	ctx.moveTo(freq*ang,0);
+	ctx.lineTo(freq*ang,radius*Math.sin(ang));
+	ctx.lineTo(radius*Math.cos(ang),radius*Math.sin(ang));
+    } else {
+	ctx.moveTo(radius*Math.cos(ang),radius*Math.sin(ang));
+    }
+    if (cbox.checked) {
+	ctx.lineTo(radius*Math.cos(ang),freq*ang);
+	ctx.lineTo(0,freq*ang);
+    }
     ctx.stroke();
     ctx.setLineDash([5,0]);
     
@@ -88,37 +108,44 @@ function draw() {
     ctx.arc(radius*Math.cos(ang),radius*Math.sin(ang),5,0,2*Math.PI);
     ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(freq*ang,radius*Math.sin(ang),5,0,2*Math.PI);
-    ctx.fill();
+    if (sbox.checked) {
+    
+	ctx.beginPath();
+	ctx.arc(freq*ang,radius*Math.sin(ang),5,0,2*Math.PI);
+	ctx.fill();
 
-    ctx.beginPath();
-    ctx.arc(freq*ang,radius*Math.sin(ang),5,0,2*Math.PI);
-    ctx.stroke();
+	ctx.beginPath();
+	ctx.arc(freq*ang,radius*Math.sin(ang),5,0,2*Math.PI);
+	ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(radius*Math.cos(ang),freq*ang,5,0,2*Math.PI);
-    ctx.fill();
+	ctx.beginPath();
+	ctx.arc(freq*ang,0,5,0,2*Math.PI);
+	ctx.fill();
 
-    ctx.beginPath();
-    ctx.arc(radius*Math.cos(ang),freq*ang,5,0,2*Math.PI);
-    ctx.stroke();
+	ctx.beginPath();
+	ctx.arc(freq*ang,0,5,0,2*Math.PI);
+	ctx.stroke();
+    }
 
-    ctx.beginPath();
-    ctx.arc(0,freq*ang,5,0,2*Math.PI);
-    ctx.fill();
+    if (cbox.checked) {
+    
+	ctx.beginPath();
+	ctx.arc(radius*Math.cos(ang),freq*ang,5,0,2*Math.PI);
+	ctx.fill();
 
-    ctx.beginPath();
-    ctx.arc(0,freq*ang,5,0,2*Math.PI);
-    ctx.stroke();
+	ctx.beginPath();
+	ctx.arc(radius*Math.cos(ang),freq*ang,5,0,2*Math.PI);
+	ctx.stroke();
 
-    ctx.beginPath();
-    ctx.arc(freq*ang,0,5,0,2*Math.PI);
-    ctx.fill();
+	ctx.beginPath();
+	ctx.arc(0,freq*ang,5,0,2*Math.PI);
+	ctx.fill();
 
-    ctx.beginPath();
-    ctx.arc(freq*ang,0,5,0,2*Math.PI);
-    ctx.stroke();
+	ctx.beginPath();
+	ctx.arc(0,freq*ang,5,0,2*Math.PI);
+	ctx.stroke();
+    }
+
 
   
     ctx.restore();
@@ -140,12 +167,22 @@ function reset(e) {
     var width = ctx.canvas.width;
 
     nang = Math.atan2(-pos.y+height/2,pos.x-width/2);
+    var wind = Math.round((nang-ang)/(2*Math.PI));
+    ang = nang - 2*Math.PI*wind;
+    if (ang > 2*Math.PI)
+	ang -= 4*Math.PI;
+    
+    if (ang < -2*Math.PI)
+	ang += 4*Math.PI;
+    angdisplay.innerHTML = Math.round(ang*100)/100;
+    /*
     if (nang > Math.PI + ang) {
 	nang -= 2*Math.PI;
     } else if (nang < ang - Math.PI) {
 	nang += 2*Math.PI;
     }
     ang = nang;
+    */
     draw();
 }
 
@@ -168,7 +205,6 @@ function doMouseUp(e) {
 }
 
 function doMouseDown(e) {
-    console.log("called");
     clicked = true;
     reset(e);
 }
